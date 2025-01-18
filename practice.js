5abb2022943882ee7328b709188f483c65f0be2e
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-01-18 06:38:55 -0800

    adding new files to practice folder

---
+++ practice.js
@@ -0,0 +1,10 @@
+// let result = document.getElementById("GFG");
+    
+// function changeColor(color) {
+//     document.body.style.background = color;
+// }
+
+// function myFunc() {
+//     changeColor('rgba(0,212,255,1');
+//     result.innerHTML = "Background Color changed";
+// }        
